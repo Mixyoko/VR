5b2e34b6352c2669d3e0bd814a6ea855d7562ff1
Mixyoko <3009yoko@gmail.com>
2025-11-21 12:00:31 +0800

    1121

---
+++ ok/data.js
@@ -0,0 +1,231 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "市民廣場",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.6307090061798224,
+        "pitch": -0.22865940404811624,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1666738804107233,
+          "pitch": 0.378263367239974,
+          "rotation": 0,
+          "target": "1-2018"
+        },
+        {
+          "yaw": 1.9314849532158531,
+          "pitch": 0.213743512581841,
+          "rotation": 0,
+          "target": "2-2020"
+        },
+        {
+          "yaw": -3.0792010301732162,
+          "pitch": 0.1844922570049583,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-2018",
+      "name": "2018年",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.32500285496056236,
+        "pitch": 0.2606612333725682,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.809002661339516,
+          "pitch": 0.4382452167943143,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": -0.03131558556691516,
+          "pitch": 0.08451772561603121,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": -1.6535394716369787,
+          "pitch": 0.20143520227366807,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-2020",
+      "name": "2020聖誕樹",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.05341461178825391,
+        "pitch": -0.42997022820671305,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.659084438436601,
+          "pitch": 0.4331593746257596,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-",
+      "name": "舞台",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -1.2118899605854558,
+        "pitch": 0.03031833717141552,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8882237243812714,
+          "pitch": 0.26836068040026895,
+          "rotation": 0,
+          "target": "1-2018"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-",
+      "name": "燈橋",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.08625477274646975,
+        "pitch": -0.2393946348458229,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.3615849814660539,
+          "pitch": 0.1820892717575795,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": -0.19410824907961022,
+          "pitch": 0.15239662122303166,
+          "rotation": 0,
+          "target": "1-2018"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
